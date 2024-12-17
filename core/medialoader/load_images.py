8475f65a987f0f49bccdb5ac973c50a7b994bcf8
dongle94 <sdy1029@gmail.com>
2024-12-17 13:13:56 +0900

    Feat: media_loader - load_images
    - __init__.py, load_images.py
    - 기존 미디어로더-이미지 그대로 첨부

---
+++ core/medialoader/load_images.py
@@ -0,0 +1,69 @@
+import os
+import cv2
+
+from core.medialoader import LoadSample
+
+IMG_FORMATS = 'bmp', 'dng', 'jpeg', 'jpg', 'mpo', 'png', 'tif', 'tiff', 'webp', 'pfm'
+
+
+class LoadImages(LoadSample):
+    def __init__(self, path, bgr=True):
+        super().__init__()
+
+        self.bgr = bgr
+
+        files = []
+        path = sorted(path) if isinstance(path, (list, tuple)) else [path]
+        for p in path:
+            p = os.path.abspath(p)
+            if '*' in p:
+                files = [os.path.join(os.path.dirname(p), f) for f in os.listdir(os.path.dirname(p))]
+            elif os.path.isdir(p):
+                files = [os.path.join(p, f) for f in os.listdir(p)]
+            elif os.path.isfile(p):
+                files.append(p)
+            else:
+                raise FileNotFoundError(f'{p} does not exist')
+
+        images = [x for x in files if x.split('.')[-1].lower() in IMG_FORMATS]
+        ni = len(images)
+
+        self.mode = 'image'
+        self.w, self.h = -1, -1
+        self.fps = -1
+        self.files = images
+        self.num_files = ni
+        self.count = 0
+
+    def __iter__(self):
+        self.count = 0
+        return self
+
+    def __next__(self):
+        if self.count == self.num_files:
+            raise StopIteration
+
+        path = self.files[self.count]
+
+        self.count += 1
+        im = cv2.imread(path)
+        assert im is not None, f'Image Not Found {path}'
+        self.h, self.w = im.shape[:2]
+        if self.bgr is False:
+            im = im[..., ::-1]
+
+        return im
+
+    def __len__(self):
+        return self.num_files  # number of files
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    p1 = './data/images/'
+    p2 = './data/images/*'
+    p3 = './data/images/sample.jpg'
+    loader = LoadImages(p1)
+    for _im in loader:
+        plt.imshow(_im)
+        plt.show()
