28dd63030479eb388759ed98788af9382ec81cf2
dongle94 <sdy1029@gmail.com>
2024-12-17 14:02:37 +0900

    Feat: media_loader - load_images 개선
    - load_images.py
    - 각 클래스 및 메소드 문서화
    - 폴더 내 이미지 리스트를 glob 모듈 활용
    - 유효 이미지 검사 효율화
    - 파일명 멤버변수 추가하여 이미지 읽을 때마다 갱신
    - 이미지 읽기 실패시 에러 처리

--- core/medialoader/load_images.py
+++ core/medialoader/load_images.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import cv2
 
 from core.medialoader import LoadSample
@@ -7,7 +8,45 @@ IMG_FORMATS = 'bmp', 'dng', 'jpeg', 'jpg', 'mpo', 'png', 'tif', 'tiff', 'webp', 
 
 
 class LoadImages(LoadSample):
+    """
+    LoadImages: A class to load image files sequentially from a given path or list of paths.
+
+    This class is an iterator that allows loading and processing of images from specified directories
+    or files. It supports filtering image files based on the extension and converting color formats
+    between BGR and RGB.
+
+    Attributes:
+        path (str or list): Path to the image folder or file(s).
+        bgr (bool): Whether to load images in BGR format (default: True).
+        files (list): List of image file paths.
+        filename (str): The current image filename being loaded.
+        num_files (int): The number of valid image files found.
+        count (int): The current index of the image being processed.
+        mode (str): Mode of operation, set to 'image'.
+        w (int): Width of the current image.
+        h (int): Height of the current image.
+        fps (int): Frame per second (default: -1, not used in image loading).
+
+    Methods:
+        __init__(self, path, bgr=True): Initializes the class with a path to images and a flag to control color format.
+        __iter__(self): Resets the iterator's count and prepares for iteration.
+        __next__(self): Loads the next image from the list and returns it.
+        __len__(self): Returns the total number of image files.
+    """
+
     def __init__(self, path, bgr=True):
+        """
+        Initialize the LoadImages class.
+
+        Args:
+            path (str or list): Path(s) to the image file(s) or folder(s).
+                It supports single file paths, directory paths, or wildcard paths.
+            bgr (bool, optional): Whether to load images in BGR format.
+                Defaults to True.
+
+        Raises:
+            FileNotFoundError: If the specified path does not exist.
+        """
         super().__init__()
 
         self.bgr = bgr
@@ -19,35 +58,54 @@ class LoadImages(LoadSample):
             if '*' in p:
                 files = [os.path.join(os.path.dirname(p), f) for f in os.listdir(os.path.dirname(p))]
             elif os.path.isdir(p):
-                files = [os.path.join(p, f) for f in os.listdir(p)]
+                files = glob.glob(os.path.join(p, '*.*'))
             elif os.path.isfile(p):
                 files.append(p)
             else:
                 raise FileNotFoundError(f'{p} does not exist')
 
-        images = [x for x in files if x.split('.')[-1].lower() in IMG_FORMATS]
+        images = [x for x in files if x.lower().endswith(IMG_FORMATS)]
         ni = len(images)
 
         self.mode = 'image'
         self.w, self.h = -1, -1
         self.fps = -1
         self.files = images
+        self.filename = "-"
         self.num_files = ni
         self.count = 0
 
     def __iter__(self):
+        """
+        Resets the iteration counter to 0 when starting a new iteration.
+
+        Returns:
+            LoadImages: The instance of the class itself.
+        """
         self.count = 0
         return self
 
     def __next__(self):
+        """
+        Load the next image in the sequence.
+
+        Returns:
+            numpy.ndarray: The loaded image in BGR or RGB format as a NumPy array.
+
+        Raises:
+            StopIteration: If all files have been processed.
+            RuntimeError: If an image cannot be loaded.
+        """
         if self.count == self.num_files:
             raise StopIteration
 
         path = self.files[self.count]
 
         self.count += 1
+        self.filename = os.path.basename(path)
         im = cv2.imread(path)
-        assert im is not None, f'Image Not Found {path}'
+        if im is None:
+            raise RuntimeError(f'Failed to load image: {path}')
         self.h, self.w = im.shape[:2]
         if self.bgr is False:
             im = im[..., ::-1]
@@ -55,15 +113,19 @@ class LoadImages(LoadSample):
         return im
 
     def __len__(self):
+        """
+        Get the total number of valid image files.
+
+        Returns:
+            int: The total number of image files.
+        """
         return self.num_files  # number of files
 
 
 if __name__ == "__main__":
-    import matplotlib.pyplot as plt
-    p1 = './data/images/'
-    p2 = './data/images/*'
-    p3 = './data/images/sample.jpg'
-    loader = LoadImages(p1)
-    for _im in loader:
-        plt.imshow(_im)
-        plt.show()
+    loader = LoadImages('./data/images/')
+    for img in loader:
+        cv2.imshow('Loaded Image', img)
+        if cv2.waitKey(1) == ord('q'):
+            break
+    cv2.destroyAllWindows()
