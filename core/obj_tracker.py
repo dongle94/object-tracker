bb267f22fd5fa35a460b08d1a522c6a780b6e9b4
dongle94 <sdy1029@gmail.com>
2024-12-19 17:19:53 +0900

    Feat: object tracker
    - obj_tracker.py
    - SORT 만을 반영한 객체감지기 작성
    - 클래스 및 함수 문서화 작업
    - tracking_id를 도출하는 행위는 track 메소드로 작성
    - main 네임스페이스에서
      - 객체 감지 결과 별도로 그리고
      - 객체 추적 결과 별도로 그림
    - cv2.waitKey 타임은 좀 더 간소화 하여 계산

---
+++ core/obj_tracker.py
@@ -0,0 +1,115 @@
+import sys
+from pathlib import Path
+
+FILE = Path(__file__).resolve()
+ROOT = FILE.parents[1]
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))
+
+from utils.logger import get_logger
+
+
+class ObjectTracker(object):
+    """
+    A generic object tracker class for integrating multiple tracking algorithms.
+
+    Attributes:
+        logger (Logger): Logger instance for debug and information logs.
+        cfg (Config): Configuration object.
+        tracker_type (str): Type of tracker used (e.g., 'sort').
+        tracker (object): Initialized tracker instance.
+    """
+    def __init__(self, cfg):
+        """
+        Initializes the ObjectTracker class.
+
+        Args:
+            cfg (Config): Configuration object containing tracker settings.
+        """
+        self.logger = get_logger()
+        self.cfg = cfg
+
+        self.tracker_type = cfg.track_model_type.lower()
+
+        if self.tracker_type == 'sort':
+            from core.sort import Sort
+            self.tracker = Sort(
+                max_age=1,
+                min_hits=3,
+                iou_threshold=0.3,
+            )
+
+    def track(self, dets):
+        """
+        Tracks objects in the current frame using the initialized tracker.
+
+        Args:
+            dets (numpy.ndarray or list): Array of detections in the format [x1, y1, x2, y2, score, class].
+
+        Returns:
+            numpy.ndarray: Array of tracked objects in the format [x1, y1, x2, y2, tracking_id].
+        """
+        if self.tracker_type == 'sort':
+            ret = self.tracker.update(dets)
+            return ret
+
+
+if __name__ == "__main__":
+    import time
+    import cv2
+
+    from utils.config import set_config, get_config
+    from utils.logger import init_logger
+    from utils.visualize import get_color_for_id
+
+    from core.obj_detector import ObjectDetector
+    from core.media_loader import MediaLoader
+
+    set_config('./configs/config.yaml')
+    _cfg = get_config()
+
+    init_logger(_cfg)
+    _logger = get_logger()
+
+    media_loader = MediaLoader(_cfg.media_source,
+                               logger=_logger,
+                               realtime=_cfg.media_realtime,
+                               bgr=_cfg.media_bgr,
+                               opt=_cfg)
+
+    _detector = ObjectDetector(cfg=_cfg)
+    _tracker = ObjectTracker(cfg=_cfg)
+
+    wt = 0 if media_loader.is_imgs else 1 / media_loader.dataset.fps
+
+    while True:
+        st = time.time()
+        frame = media_loader.get_frame()
+
+        _det = _detector.run(frame)
+
+        for d in _det:
+            x1, y1, x2, y2 = map(int, d[:4])
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (96, 96, 216), thickness=2, lineType=cv2.LINE_AA)
+
+        # tracking update
+        if len(_det):
+            track_ret = _tracker.track(_det)
+            for t in track_ret:
+                x1, y1, x2, y2 = map(int, t[:4])
+                tracking_id = int(t[4])
+                color = get_color_for_id(tracking_id)
+                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), color, thickness=2)
+                cv2.putText(frame, f"ID: {int(tracking_id)}", (int(x1), int(y1) - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, thickness=1)
+
+        et = time.time()
+        if media_loader.is_imgs:
+            delay = 0
+        else:
+            delay = int(max(1, (wt - (et - st)) * 1000)) if et - st < wt else 1
+
+        cv2.imshow('_', frame)
+        if cv2.waitKey(delay) == ord('q'):
+            print("-- CV2 Stop --")
+            break
